04f936acd07f96c23087fc0677c5612f71bb0a4d
stasiukevych.a <stasiukevych.a@gmail.com>
2021-04-04 23:19:23 +0300

    added deleting

--- src/main/java/handler/sql/DeleteSQLExecutor.java
+++ src/main/java/handler/sql/DeleteSQLExecutor.java
@@ -1,9 +1,28 @@
 package handler.sql;
 
+import java.io.Console;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import logging.SessionLogger;
 
 public class DeleteSQLExecutor {
   public static void deletePassword(final Connection connection) {
-
+    String exit;
+    do {
+      final Console console = System.console();
+      final String DELETE_WITH_CONDITION = "delete from password where service = ?";
+      final String service = console.readLine(">_ print service for deleting: ");
+      try {
+        final PreparedStatement statement = connection.prepareStatement(DELETE_WITH_CONDITION);
+        statement.setString(1, service);
+        statement.execute();
+        console.printf(String.format(">_ %s was deleted\n", service));
+      } catch (SQLException throwable) {
+        new SessionLogger().error(throwable.getMessage());
+        throwable.printStackTrace();
+      }
+      exit = console.readLine(">_ continue deleting (y-yes,n-no): ");
+    } while (exit.equalsIgnoreCase("y"));
   }
 }
